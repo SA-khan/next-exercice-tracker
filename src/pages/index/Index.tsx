ed6a4dd8f7881edae73c4d377be4023f7470a720
Saad Ahmed <saadahmed7262@gmail.com>
2023-03-22 14:55:00 +0500

    index commented out

--- src/pages/index/Index.tsx
+++ src/pages/index/Index.tsx
@@ -5,17 +5,19 @@ import Layout from '@/components/Layout';
 
 const Index = () => {
 
-    function addZero(i) {
-        if (i < 10) {i = "0" + i}
-        return i;
-      }
+    // function addZero(i) {
+    //     if (i < 10) {i = "0" + i}
+    //     return i;
+    //   }
       
-      const d = new Date();
-      let h = addZero(d.getHours());
-      let m = addZero(d.getMinutes());
-      let s = addZero(d.getSeconds());
-      let symbol = h / 12 > 0 ? 'pm' : 'am' 
-      let time = h % 12 + ":" + m;
+    //   const d = new Date();
+    //   let h = addZero(d.getHours());
+    //   let m = addZero(d.getMinutes());
+    //   let s = addZero(d.getSeconds());
+    //   let symbol = h / 12 > 0 ? 'pm' : 'am' 
+    //   let time = h % 12 + ":" + m;
+    let time = ''
+    let symbol = ''
 
     return <React.Fragment>
         <PageHeading title='Home' source='home.jfif' />  
